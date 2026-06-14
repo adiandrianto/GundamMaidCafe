fa250ebcb9ee8775a2b198fb7f274e76a741da9c
Adi Andrianto <adiandrianto@gmail.com>
2026-06-13 22:51:39 -0400

    add table prefab

---
+++ Resources/Levels/level_base.gd
@@ -0,0 +1,7 @@
+extends Node
+class_name LevelRes
+
+@export var level: int
+@export var total_table: int
+@export var total_goal: int
+@export var level_duration: float #berapa detik waktunya
---
+++ Scripts/table.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+@onready var maid_spot: Marker2D = %MaidSpot
+@onready var customer_spot: Marker2D = %CustomerSpot
