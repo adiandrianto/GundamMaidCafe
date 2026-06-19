66c0e8ac8dcbbf75b7c2b13445a11051ecd65c7d
Bryan R <leinnocent0@gmail.com>
2026-06-19 18:07:07 +0700

    tweak (ordering): tweaked test scene

--- Scripts/UI/maidlist_item.gd
+++ Scripts/UI/maidlist_item.gd
@@ -5,9 +5,4 @@ extends TextureButton
 
 @onready var icon: TextureRect = $MaidIcon
 
-func _get_drag_data(at_position: Vector2) -> Maid:
-	if icon.texture == null:
-		return
-	var preview = duplicate()
-	set_drag_preview(preview)
-	return maid
+
